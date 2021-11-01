4e4409af42ab8ebb858e66c4c24d73b7222bc929
Ouss84 <bahrioussama84@gmail.com>
2021-11-01 14:40:32 +0200

    passing the images to the AnimalSingle

--- src/components/Animals/AnimalSingle.js
+++ src/components/Animals/AnimalSingle.js
@@ -3,6 +3,17 @@ import { useParams } from "react-router-dom";
 
 const AnimalSingle = () => {
   let { animal } = useParams();
-  return <div> This is the page for {animal}</div>;
+  console.log(animal);
+  return (
+    <div>
+      <div>This is the page for {animal}</div>
+      <div className="animal-details">
+        <img
+          src={`https://source.unsplash.com/1600x900/?${animal}`}
+          alt="animal-pic"
+        />
+      </div>
+    </div>
+  );
 };
 export default AnimalSingle;
